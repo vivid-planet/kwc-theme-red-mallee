3a9ccf62b6819eb0ace705066f9165fea9f6c050
Daniel Bacher <db@vivid-planet.com>
2013-12-09 09:16:04 +0100

    Added Component for BasicTable and scss

---
+++ PriceTable/Component.php
@@ -0,0 +1,16 @@
+<?php
+class Theme_PriceTable_Component extends Kwc_Basic_Table_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['tableStyles'] = array('standard' => trlKwf('Standard'));
+        return $ret;
+    }
+    
+    public function getTemplateVars()
+    {
+        $ret = parent::getTemplateVars();
+        return $ret;
+    }
+}
