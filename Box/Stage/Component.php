abc5d4d382fcd72380dce495cc24b196b00777ba
Michael Freudenthaler <mf@vivid-planet.com>
2014-01-08 14:18:32 +0100

    component.js wird automatisch gefunden

--- Box/Stage/Component.php
+++ Box/Stage/Component.php
@@ -6,7 +6,6 @@ class Theme_Box_Stage_Component extends Kwc_Abstract_List_Component
         $ret = parent::getSettings();
         $ret['componentName'] = trlKwfStatic('Stage');
         $ret['generators']['child']['component'] = 'Theme_Box_Stage_Image_Component';
-        $ret['assets']['files'][] = 'web/themes/Theme/Box/Stage/Component.js';
         $ret['assets']['files'][] = 'web/themes/Theme/Box/Stage/Hammer.js';
         $ret['assets']['files'][] = 'web/themes/Theme/Box/Stage/Carousel.js';
         $ret['assets']['files'][] = 'web/themes/Theme/Box/Stage/NextPreviousLinks.js';
