3b14b035654a8815544f8844cbeb39a086a10141
Daniel Bacher <db@vivid-planet.com>
2014-04-03 16:36:04 +0200

    Link und Link-list abgewandelt wegen design

---
+++ Link/Component.php
@@ -0,0 +1,9 @@
+<?php
+class Theme_Link_Component extends Kwc_Basic_Link_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        return $ret;
+    }
+}
---
+++ List/Links/Component.php
@@ -0,0 +1,9 @@
+<?php
+class Theme_List_Links_Component extends Kwc_List_Links_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        return $ret;
+    }
+}
